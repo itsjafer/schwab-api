37632eff6e77dd5008eefefcf884e1e87f79734e
Jafer Haider <itsjafer@gmail.com>
2024-01-03 13:16:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="schwab_api",
     packages=setuptools.find_packages(),
-    version="0.3.7",
+    version="0.3.8",
     license="MIT",
     description="Unofficial Schwab API wrapper in Python 3.",
     author="Jafer Haider",
